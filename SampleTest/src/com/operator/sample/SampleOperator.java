a36d7007cc8a8d2de50069749c975be3ae799f08
pkrsoma <praveenkumar.soma2312@gmail.com>
2022-12-28 15:42:04 -0500

    All operators examples

--- SampleTest/src/com/operator/sample/SampleOperator.java
+++ SampleTest/src/com/operator/sample/SampleOperator.java
@@ -23,24 +23,29 @@ public class SampleOperator {
 				System.out.println(x<y&&x>y);
 				System.out.println(x>y&x<y);
 				System.out.println(x>y||x<y);
-				System.out.println(x<y|x>y);
+				System.out.println(x<y|x>y);		
+				int a=2;
+				int b=7;
+				int min=(a<b)?a:b;
+				System.out.println(min);
+				int max=(a>b)?a:b;
+				System.out.println(max);
+				a+=7;
+				b-=9;
+				System.out.println(a);
+				System.out.println(b);
+				b-=5;
+				b+=15;
+				b*=10;
+				b/=10;
+				System.out.println(b);
+				long h=123423546;
+				long j=2346546;
+				long k;
+				k=(long)(h+j);
+				System.out.println(k);
 				
 				
-				
-				
-				
-				
-
-aaaaaaa
-
-
-
-
-
-				
-			
-
-
 	}
 
 }
